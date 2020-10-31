b59fd715b69435cc295d4c0258da1820aee67e0a
Supuni22 <supunimanamperi@gmail.com>
2020-10-31 09:44:32 +0530

    Created initial setup

--- covid/src/App.js
+++ covid/src/App.js
@@ -1,9 +1,36 @@
+import React from 'react'
 import './App.css'
+import { FormControl,Select, MenuItem } from '@material-ui/core';
 
 const App = () => {
   return (
     <div className="App">
-      <h2> Covid App</h2>
+      {/* Header */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+
+          {/* Loopthrough all of the countries and show all the countries */}
+          <Select variant ="outlined" value="abc">
+            <MenuItem value='worldwide'> Worlwide </MenuItem>
+            <MenuItem value='worldwide'> Option Two </MenuItem>
+            <MenuItem value='worldwide'> Option Three </MenuItem>
+            <MenuItem value='worldwide'> Option Four </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+     
+      
+      {/* Title + Select input dropdown field */}
+
+      {/* InforBoxes */}
+      {/* InforBoxes */}
+      {/* InforBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   )
 }
